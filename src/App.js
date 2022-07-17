5748259e85193a9c0959689ed05804963f059418
dimachilaia <92372285+dimachilaia@users.noreply.github.com>
2022-07-17 15:42:28 +0400

    deploy my react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import './App.css';
+import React,{useState, useEffect} from 'react'
 
-function App() {
+import Form from "./components/Form"
+import TodoList from "./components/TodoList"
+
+const App = () => {
+ 
+  const [inputText, setInputText] = useState('')
+  const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState('all')
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+
+  useEffect(()=>{
+    filterHandler()
+  },[todos, status])
+
+
+  const filterHandler = (e)=>{
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter((todo)=> todo.completed === true))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter((todo)=>todo.completed === false))
+        break;
+      default:
+         setFilteredTodos(todos)
+        break
+    }
+  }
+
   return (
-    <div>
-   
-    </div>
-  );
+   <div className='App'>
+    <header>
+      <h1>Todo List </h1>
+    </header>
+    <Form setInputText={setInputText} todos={todos} setTodos={setTodos} inputText = {inputText} setStatus={setStatus} />
+    <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos}/>
+
+   </div>
+ )
 }
 
-export default App;
+export default App
